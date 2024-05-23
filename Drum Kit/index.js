f28586607f57e33c287e8a7fefa2504128bca4c4
Denis Nikulin <nikulinden@gmail.com>
2024-05-23 09:12:39 -0700

    added event listener to all buttons

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,4 +1,6 @@
-document.querySelector("").addEventListener("click", handleClick);
+for (var i = 0; i<document.querySelectorAll(".drum").length; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
 
 
 function handleClick(){
